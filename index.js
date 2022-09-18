108396b10e79c959ff1794abeef4858c339b89ff
Naveen <nvnkms123@gmail.com>
2022-09-18 11:25:12 +0530

    second commit

--- index.js
+++ index.js
@@ -1,65 +1,32 @@
-//============================ HEADER-SECTION ==================================
+//========================= NAV-SECTION ====================
+// function to animate resume button
+const resumeBtn = document.querySelector(".span");
+// function definition
+const addYellow = () => {
+  resumeBtn.classList.add("span-transition");
+}
 
-// to add color to first nav link before scrolling
-const firstChild = document.querySelector(".first-navchild");
+const removeYellow = () => {
+  resumeBtn.classList.remove("span-transition");
+}
+// function call
+// resumeBtn.onmouseover = animateBtn
+// function to show menubar on smallscreen
+const menu = document.querySelector(".menu");
+const closeBtn = document.querySelector(".close");
+const hamburger = document.querySelector(".hamburger");
+// function definition
+const displayMenu = () => {
+  menu.classList.add("menu-transition")
+}
+// function call
+hamburger.onclick = displayMenu;
 
-firstChild.classList.add("nav-active");
+// function definition
+const closeMenu = () => {
+  menu.classList.remove("menu-transition")
+}
 
-// Function to change color of navigation links during scroll through each section
-document.getElementById("main").addEventListener("scroll", () => {
+// function call
 
-  // Function definition
-  const changeNavColor = (section, index) => {
-    const sectionPositionTop = section.getBoundingClientRect().top;
-
-    const sectionPositionBottom = section.getBoundingClientRect().bottom;
-
-    const screenPosition = window.innerHeight;
-
-    const list = document.querySelectorAll(".nav_link")[index].classList;
-
-    // add class when section top is above half of the screen and remove when not
-    sectionPositionTop < screenPosition / 2
-      ? list.add("nav-active")
-      : list.remove("nav-active");
-
-    // remove class when section bottom is above half of the screen
-    sectionPositionBottom < screenPosition / 2 && list.remove("nav-active");
-  };
-
-  const sections = document.querySelectorAll("section");
-
-  //   function call
-  sections.forEach(changeNavColor);
-
-});
-
-
-// var styles = window.getComputedStyle(element,':before')
-// 
-
-// const element = document.getElementById("test");
-
-// console.log(value);
-
-// for (x in cssObj) { 
-//     let text = "";
-//   cssObjProp = cssObj.item(x)
-//   text += cssObjProp + " = " + cssObj.getPropertyValue(cssObjProp);
-//   console.log(text);
-// }
-
-
-
-// var content = styles['content'];
-// styles.style.border="none";
-
-// let d = document.querySelector('#about h2')
-// d.addEventListener('animationend', function(ev) {
-//    if (ev.animationName === 'typing') {
-//     var element = document.querySelector('#about h2');
-//     const cssObj = window.getComputedStyle(element, ':before');
-    
-//      cssObj["border-left-width"] = "0px";
-//    }
-// })
+closeBtn.onclick = closeMenu;
