593c5e6a3e5e3ba51c75597cbd7b2375521c3ee6
Naveen <nvnkms123@gmail.com>
2024-10-31 17:49:56 +0530

    update resume

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-//========================= SCROLL ANIMATION ====================
 let observer = new IntersectionObserver( (entries) => {
   entries.forEach( (entry) => {
     if (entry.isIntersecting) {
@@ -13,10 +12,8 @@ let target = document.querySelectorAll(".hidden");
 target.forEach( (el) => observer.observe(el))
 
 
-//========================= NAV-SECTION ====================
-//====== function to animate resume button ======
 const resumeBtn = document.querySelector(".span");
-// function definition
+
 const addYellow = () => {
   resumeBtn.classList.add("span-transition");
 }
@@ -26,24 +23,21 @@ const removeYellow = () => {
 }
 
 
-//====== function to show menubar on smallscreen ======
 const menu = document.querySelector(".menu");
 const closeBtn = document.querySelector(".close");
 const hamburger = document.querySelector(".hamburger");
 const linkElements =document.querySelectorAll(".menu ul li")
-// function definition
+
 const displayMenu = () => {
   menu.classList.add("menu-transition");
   
 }
-// function call
+
 hamburger.onclick = displayMenu;
 
-// function definition
 const closeMenu = () => {
   menu.classList.remove("menu-transition")
 }
 
-// function call
 closeBtn.onclick = closeMenu;
 linkElements.forEach((link) => link.onclick = closeMenu);
